a91416739f6509c2cba2aaaec9e9c6c3526a34fb
IlkerBasar <ilkerryasinbasar@gmail.com>
2025-06-01 19:41:42 +0300

    id kontrolü kategori için yapıldı.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -21,18 +23,18 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
-        public ProductManager(IProductDal productDal)
+        ILogger _logger;
+        public ProductManager(IProductDal productDal, ILogger logger)
         {
             _productDal = productDal;
+            _logger = logger;
         }
 
+        //[ValidationAspect(typeof(ProductValidator))]  
         public IResult Add(Product product)
         {
             //business codes : iş kodları
             //validation : doğrulama 
-
-            ValidationTool.Validate(new ProductValidator(),product);
-
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
